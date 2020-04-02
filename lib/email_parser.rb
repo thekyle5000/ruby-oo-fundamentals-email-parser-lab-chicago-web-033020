5d1ce03b4f91f3ca21e3800363014c6f7a21ace5
Kyle Bradley <kylepersonalmail@gmail.com>
2020-04-02 02:12:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,8 +12,7 @@ attr_accessor :emails
   
   def parse
     if
-      @emails.include?(" ")
-      @emails.split(",").uniq
+      @emails.include?(",").split(",").uniq
     else 
       @emails.split(" ").uniq
     end
